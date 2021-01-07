04b1eb39f3f370d18d6345035405bbda28dcad4a
Mai Van Manh <mvmanh@mvm.local>
2021-01-07 16:20:26 +0700

    update

--- lab01/echo.php
+++ lab01/echo.php
@@ -2,14 +2,26 @@
     echo "Số biến có trong GET: " . count($_GET) . "<br>";
     echo "<ul>";
     foreach ($_GET as $key => $value) {
-        echo "<li>$key: " . $_GET[$key] . "</li>";
+        echo "<li>$key: ";
+        if (is_array($_GET[$key])) {
+            print_r($_GET[$key]);
+        }else {
+            echo $_GET[$key];
+        }
+        echo "</li>";
     }
     echo "</ul>";
 
     echo "<br><br>Số biến có trong POST: " . count($_POST) . "<br>";
     echo "<ul>";
     foreach ($_POST as $key => $value) {
-        echo "<li>$key: " . $_POST[$key] . "</li>";
+        echo "<li>$key: ";
+        if (is_array($_POST[$key])) {
+            print_r($_POST[$key]);
+        }else {
+            echo $_GET[$key];
+        }
+        echo "</li>";
     }
     echo "</ul>";
 ?>
